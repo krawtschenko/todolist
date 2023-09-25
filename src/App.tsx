f4eb7d8d8ba75cebef9be1bde0ce56a749dc9356
krawtschenko <eugenykravchenko@gmail.com>
2023-09-25 20:49:54 +0200

    3 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./components/TodoList";
+// import * as crypto from "crypto";
 
 export const App = () => {
 	const todoListTitle1: string = "What to do"
 
-	const [tasks, setTasks] = useState<Array<TaskType>>([
-		{id: 1, isDone: true, title: "HTML&CSS"},
-		{id: 2, isDone: true, title: "JavaScript"},
-		{id: 3, isDone: false, title: "React"}
+	const [tasks, setTasks] = useState<TaskType[]>([
+		{id: crypto.randomUUID(), isDone: true, title: "HTML&CSS"},
+		{id: crypto.randomUUID(), isDone: true, title: "JavaScript"},
+		{id: crypto.randomUUID(), isDone: false, title: "React"}
 	])
 
 	const [filter, setFilter] = useState<FilterType>(FilterType.All)
 
-	const removeTask = (idTask: number) => {
+	const removeTask = (idTask: string) => {
 		setTasks(tasks.filter(el => el.id !== idTask))
+	}
+
+	const addTask = (title: string) => {
+		setTasks([...tasks, {id: crypto.randomUUID(), isDone: false, title}])
 	}
 
 	const changeFilter = (filter: FilterType) => {
@@ -32,13 +37,14 @@ export const App = () => {
 
 	return (
 		<div className="App">
-			<TodoList tasks={filteredTasks} title={todoListTitle1} removeTask={removeTask} filters={changeFilter}/>
+			<TodoList tasks={filteredTasks} title={todoListTitle1} removeTask={removeTask} filters={changeFilter}
+			          addTask={addTask}/>
 		</div>
 	);
 }
 
 export interface TaskType {
-	id: number
+	id: string
 	title: string
 	isDone: boolean
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,7 @@
-import React, {FC} from "react";
+import React, {ChangeEvent, FC, useState} from "react";
 import {FilterType, TaskType} from "../App";
 
-export const TodoList: FC<TodoListProps> = ({title, tasks, removeTask, filters}) => {
+export const TodoList: FC<TodoListProps> = ({title, tasks, removeTask, filters, addTask}) => {
 	const allTasks = tasks.length ? tasks.map(el => {
 		return (
 			<li key={el.id}>
@@ -11,12 +11,28 @@ export const TodoList: FC<TodoListProps> = ({title, tasks, removeTask, filters})
 		)
 	}) : <span>Have no tasks</span>
 
+	const [input, setInput] = useState<string>('')
+
+	const changeInput = (value: ChangeEvent<HTMLInputElement>) => {
+		setInput(value.currentTarget.value)
+	}
+
+	const addTaskFn = () => {
+		const titleNewTask = input.trim()
+		if (titleNewTask && titleNewTask.length < 20) {
+			addTask(input)
+		}
+		setInput('')
+	}
+
 	return (
 		<div className="todoList">
 			<h3>{title}</h3>
 			<div>
-				<input/>
-				<button>+</button>
+				<input value={input} onChange={(event) => changeInput(event)}/>
+				<button disabled={input.trim().length > 20} onClick={addTaskFn}>+</button>
+				{/*<div><span>{input.trim().length > 20 || !input.trim() ? 'Error' : ''}</span></div>*/}
+				<div><span>{input.trim().length > 20 ? 'So long' : 'Enter title'}</span></div>
 			</div>
 			<ul>
 				{allTasks}
@@ -33,6 +49,7 @@ export const TodoList: FC<TodoListProps> = ({title, tasks, removeTask, filters})
 interface TodoListProps {
 	title: string
 	tasks: Array<TaskType>
-	removeTask: (idTask: number) => void
+	removeTask: (idTask: string) => void
 	filters: (filter: FilterType) => void
+	addTask: (title: string) => void
 }
